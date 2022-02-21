be9caa045a7b1966df7ee86a2176b711d57825fd
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-21 18:53:17 +0700

    init

--- validate-form/src/Components/SignupForm.tsx
+++ validate-form/src/Components/SignupForm.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
+import { Email, Name, Phone, Password, confirmedPassword } from "./interface";
 
 const SignupForm: React.FC = () => {
+  const [email, setEmail] = useState<Email[]>([]);
+  const [name, setName] = useState<Name[]>([]);
+  const [phone, setPhone] = useState<Phone[]>([]);
+  const [password, setPassword] = useState<Password[]>([]);
+  const [confirmedPassword, setConfirmedPassword] = useState<
+    confirmedPassword[]
+  >([]);
   return <>Sign up form</>;
 };
 
