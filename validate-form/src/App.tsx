fd143f6cd4f0d3d3cd302fbf5e06f360f2596c40
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-21 18:36:30 +0700

    init

--- validate-form/src/App.tsx
+++ validate-form/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import SignupForm from "./Components/SignupForm";
 
 const App: React.FC = () => {
-  return <div className="App">Nguyen Van Trung</div>;
+  return (
+    <div className="App">
+      <header>Sign up</header>
+      <SignupForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ validate-form/src/Components/SignupForm.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./signup.css";
+
+const SignupForm = () => {
+  return <>Sign up form</>;
+};
+
+export default SignupForm;
