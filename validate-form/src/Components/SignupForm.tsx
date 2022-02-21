06d66f64d8fd0642059f62284d57b96f5259e80e
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-21 21:46:02 +0700

    init

--- validate-form/src/Components/SignupForm.tsx
+++ validate-form/src/Components/SignupForm.tsx
@@ -4,13 +4,56 @@ import { Email, Name, Phone, Password, confirmedPassword } from "./interface";
 
 const SignupForm: React.FC = () => {
   const [email, setEmail] = useState<Email[]>([]);
-  const [name, setName] = useState<Name[]>([]);
+  const [name, setName] = useState<Name | any>("");
   const [phone, setPhone] = useState<Phone[]>([]);
   const [password, setPassword] = useState<Password[]>([]);
   const [confirmedPassword, setConfirmedPassword] = useState<
     confirmedPassword[]
   >([]);
-  return <>Sign up form</>;
+
+  return (
+    <section>
+      <form className="infoform">
+        <label> Your name </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <label> Email address </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+        <label> Password </label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+        <label> Confirm Password </label>
+        <input
+          type="text"
+          id="confirmedPassword"
+          name="confirmedPassword"
+          placeholder="Confirm your password"
+        />
+        <label> Phone number </label>
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          placeholder="Enter your phone numbers"
+        />
+        <button type="submit"> Continue </button>
+      </form>
+    </section>
+  );
 };
 
 export default SignupForm;
